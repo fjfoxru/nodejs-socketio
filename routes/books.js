522ccf34fdc13ac56636abc18ddcf7e3e4139866
fjfoxru <61179622+fjfoxru@users.noreply.github.com>
2021-01-30 23:17:52 +0300

    Update books.js

--- routes/books.js
+++ routes/books.js
@@ -2,13 +2,6 @@ const express = require('express');
 const router = express.Router();
 const {Book} = require('../models');
 
-const http = require('http');
-const socketIO = require('socket.io');
-
-const app = express();
-const server = http.Server(app);
-const io = socketIO(server);
-
 router.get('/', async (request, responce) => {
     const books = await Book.find();
     responce.render("books/index", {
@@ -56,38 +49,6 @@ router.get('/:id', async (req, res) => {
     res.render("books/view", {
         title: "Просмотр книги",
         book: book,
-    });
-
-    io.on('connection', (socket) => {
-        const {id} = socket;
-        console.log(`Socket connected: ${id}`);
-    
-        // сообщение себе
-        socket.on('message-to-me', (msg) => {
-            msg.type = 'me';
-            socket.emit('message-to-me', msg);
-        });
-    
-        // сообщение для всех
-        socket.on('message-to-all', (msg) => {
-            msg.type = 'all';
-            socket.broadcast.emit('message-to-all', msg);
-            socket.emit('message-to-all', msg);
-        });
-    
-        // работа с комнатами
-        const {roomName} = socket.handshake.query;
-        console.log(`Socket roomName: ${roomName}`);
-        socket.join(roomName);
-        socket.on('message-to-room', (msg) => {
-            msg.type = `room: ${roomName}`;
-            socket.to(roomName).emit('message-to-room', msg);
-            socket.emit('message-to-room', msg);
-        });
-    
-        socket.on('disconnect', () => {
-            console.log(`Socket disconnected: ${id}`);
-        });
     });
 });
 
